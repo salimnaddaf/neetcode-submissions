0f8d57d84a675714d159419232a3b3470529fb75
SaL!M Naddaf <salimnaddaf10@gmail.com>
2026-08-30 11:50:22 +0300

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        st=""
+        for ch in s:
+            if( ord(ch)>=ord('a') and ord(ch)<=ord('z') or ord(ch)>=ord('A') and ord(ch)<=ord('Z') or ord(ch)>=ord('0') and ord(ch)<=ord('9')):
+                st+=ch
+        left=0
+        right=len(st)-1
+        while right>left:
+            if st[left]!=st[right]:
+                return False
+            left+=1
+            right-=1
+        return True
