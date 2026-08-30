2de96fe50d41ec7fc309e610422ec538fa993fe0
SaL!M Naddaf <salimnaddaf10@gmail.com>
2026-08-30 11:47:12 +0300

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s=s.lower()
+        st=''
+        for ch in s:
+            st+=(ch if ch.isalpha() else '')
+        left=0
+        right=len(st)-1
+        while right>left:
+            if st[left]!=st[right]:
+                return False
+            left+=1
+            right-=1
+        return True
