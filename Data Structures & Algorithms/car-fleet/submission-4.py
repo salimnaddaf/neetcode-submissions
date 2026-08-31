b84c817ccef7b652affa72caa38e18aba440d533
SaL!M Naddaf <salimnaddaf10@gmail.com>
2026-08-31 12:23:47 +0300

    Add: car-fleet - submission-4

---
+++ Data Structures & Algorithms/car-fleet/submission-4.py
@@ -0,0 +1,14 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        pairs = []
+        stack = []
+        for i in range(len(position)):
+            pairs.append([position[i],speed[i]])
+        pairs.sort()
+        for p in pairs[-1::-1]:
+            distanceLeft = target - p[0]
+            timeRemaining = distanceLeft / p[1]
+            if not stack or stack[-1] < timeRemaining:
+                stack.append(timeRemaining)
+
+        return len(stack)
